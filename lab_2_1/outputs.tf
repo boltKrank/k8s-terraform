59715075bcf654e0d1899418fdd66e0524508329
Simon Anderson <simon.anderson111@gmail.com>
2021-04-28 22:50:07 +1000

    removed outputs dupes

--- lab_2_1/outputs.tf
+++ lab_2_1/outputs.tf
@@ -1,26 +1,26 @@
 #k8s master
-output "public_instance_id" {
+output "k8s_master_id" {
   value = aws_instance.k8s_master.id
 }
 
-output "public_instance_ip" {
+output "k8s_master_public_ip" {
   value = aws_instance.k8s_master.public_ip
 }
 
 #k8s worker 1
-output "public_instance_id" {
+output "k8s_worker1_id" {
   value = aws_instance.k8s_worker1.id
 }
 
-output "public_instance_ip" {
+output "k8s_worker1_public_ip" {
   value = aws_instance.k8s_worker1.public_ip
 }
 
 #k8s worker 2
-output "public_instance_id" {
+output "k8s_worker2_id" {
   value = aws_instance.k8s_worker2.id
 }
 
-output "public_instance_ip" {
+output "k8s_worker2_public_ip" {
   value = aws_instance.k8s_worker2.public_ip
 }
